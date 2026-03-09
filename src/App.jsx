3679efcee023fff6ac8eb0910c56e8b19c5b0211
aee4 <eeyramagbetor@gmail.com>
2026-03-09 20:44:04 +0000

    feat: loadtime

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/layout/Layout";
-
-import Home from "./pages/Home";
-import Explore from "./pages/Explore";
-import AssetDetail from "./pages/AssetDetail";
-import Learn from "./pages/Learn";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import SignUpType from "./pages/SignUpType";
 import ScrollToTop from "./components/common/ScrollToTop";
+import PageLoader from "./components/common/PageLoader";
+
+// Lazy loaded pages
+const Home = lazy(() => import("./pages/Home"));
+const Explore = lazy(() => import("./pages/Explore"));
+const AssetDetail = lazy(() => import("./pages/AssetDetail"));
+const Learn = lazy(() => import("./pages/Learn"));
+const SignIn = lazy(() => import("./pages/SignIn"));
+const SignUp = lazy(() => import("./pages/SignUp"));
+const SignUpType = lazy(() => import("./pages/SignUpType"));
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Routes>
+      <Suspense fallback={<PageLoader />}>
+        <Routes>
 
-        {/* Pages with Navbar + Footer */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/assets/:id" element={<AssetDetail />} />
-          <Route path="/learn" element={<Learn />} />
-        </Route>
+          {/* Pages with Navbar + Footer */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/assets/:id" element={<AssetDetail />} />
+            <Route path="/learn" element={<Learn />} />
+          </Route>
 
-        {/* Auth pages WITHOUT layout */}
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUpType />} />
-        <Route path="/signup/details" element={<SignUp />} />
+          {/* Auth pages WITHOUT layout */}
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUpType />} />
+          <Route path="/signup/details" element={<SignUp />} />
 
-      </Routes>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
 
-export default App;
+export default App;
